cf76525903a6c1272b69e0814826f958dd40376f
Nicolas Adell <81983008+NicolasAdell@users.noreply.github.com>
2024-09-17 10:30:28 -0300

    feat: add animation file

--- workspace_lpc845/01_animation/source/01_animation.c
+++ workspace_lpc845/01_animation/source/01_animation.c
@@ -4,6 +4,8 @@
 #include "fsl_debug_console.h"
 #include <stdbool.h>
 
+#define DIGIT_1 8
+#define DIGIT_2 9
 #define SEGMENT_A 11
 #define SEGMENT_B 10
 #define SEGMENT_C 6
@@ -25,6 +27,8 @@ int main(void) {
     gpio_pin_config_t out_config = {kGPIO_DigitalOutput, 1};
 
     // Configure segments as outputs
+    GPIO_PinInit(GPIO, 0, DIGIT_1, &out_config);
+    GPIO_PinInit(GPIO, 0, DIGIT_2, &out_config);
     GPIO_PinInit(GPIO, 0, SEGMENT_A, &out_config);
     GPIO_PinInit(GPIO, 0, SEGMENT_B, &out_config);
     GPIO_PinInit(GPIO, 0, SEGMENT_C, &out_config);
@@ -40,28 +44,39 @@ int main(void) {
 
     while (true) {
     	// Read USER button in Pull up
-    	if (!GPIO_PinRead(GPIO, 0, USER_BUTTON)){
+    	while (!GPIO_PinRead(GPIO, 0, USER_BUTTON)){
+			GPIO_PinWrite(GPIO, 0, DIGIT_1, false);
+			GPIO_PinWrite(GPIO, 0, DIGIT_2, false);
     		// Turn each segment on to create an animation (Common anode)
 			GPIO_PinWrite(GPIO, 0, SEGMENT_A, false);
 			for (uint32_t i = 0; i < 250000; i++);
-			GPIO_PinWrite(GPIO, 0, SEGMENT_B, false);
-			for (uint32_t i = 0; i < 250000; i++);
+			GPIO_PinWrite(GPIO, 0, SEGMENT_A, true);
 			GPIO_PinWrite(GPIO, 0, SEGMENT_C, false);
 			for (uint32_t i = 0; i < 250000; i++);
+			GPIO_PinWrite(GPIO, 0, SEGMENT_C, true);
 			GPIO_PinWrite(GPIO, 0, SEGMENT_D, false);
 			for (uint32_t i = 0; i < 250000; i++);
+			GPIO_PinWrite(GPIO, 0, SEGMENT_D, true);
 			GPIO_PinWrite(GPIO, 0, SEGMENT_E, false);
 			for (uint32_t i = 0; i < 250000; i++);
-			GPIO_PinWrite(GPIO, 0, SEGMENT_F, false);
-    	} else {
-    		// Turn all segments off (Common Anode)
-			GPIO_PinWrite(GPIO, 0, SEGMENT_A, true);
-			GPIO_PinWrite(GPIO, 0, SEGMENT_B, true);
-			GPIO_PinWrite(GPIO, 0, SEGMENT_C, true);
-			GPIO_PinWrite(GPIO, 0, SEGMENT_D, true);
 			GPIO_PinWrite(GPIO, 0, SEGMENT_E, true);
+			GPIO_PinWrite(GPIO, 0, SEGMENT_F, false);
+			for (uint32_t i = 0; i < 250000; i++);
 			GPIO_PinWrite(GPIO, 0, SEGMENT_F, true);
+			GPIO_PinWrite(GPIO, 0, SEGMENT_B, false);
+			for (uint32_t i = 0; i < 250000; i++);
+			GPIO_PinWrite(GPIO, 0, SEGMENT_B, true);
     	}
+
+		GPIO_PinWrite(GPIO, 0, DIGIT_1, true);
+		GPIO_PinWrite(GPIO, 0, DIGIT_2, true);
+		// Turn all segments off (Common Anode)
+		GPIO_PinWrite(GPIO, 0, SEGMENT_A, true);
+		GPIO_PinWrite(GPIO, 0, SEGMENT_B, true);
+		GPIO_PinWrite(GPIO, 0, SEGMENT_C, true);
+		GPIO_PinWrite(GPIO, 0, SEGMENT_D, true);
+		GPIO_PinWrite(GPIO, 0, SEGMENT_E, true);
+		GPIO_PinWrite(GPIO, 0, SEGMENT_F, true);
 
     	// Waiting to deal with bounce
     	for (uint32_t i = 0; i < 250000; i++);
